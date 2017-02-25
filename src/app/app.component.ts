eec36a6c98d83ed8d232a7aab1f3c7ba1573391d
vunguyenhung <vunguyenhung@outlook.com>
2017-02-26 03:42:41 +0700

    * implemented remember me feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import {Component} from "@angular/core";
 import {Router} from "@angular/router";
 import "rxjs/add/operator/map";
 import {PageScrollConfig} from "ng2-page-scroll";
+import {AppState} from "./shared/store/reducers/app.reducer";
+import {Store} from "@ngrx/store";
+import {DataAction} from "./shared/store/actions/data.action";
+import {JwtHelper} from "angular2-jwt";
+import {Account} from "./shared/models/account.model";
 
 @Component({
   selector: 'app-root',
@@ -17,8 +22,21 @@ import {PageScrollConfig} from "ng2-page-scroll";
 })
 export class AppComponent {
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private store: Store<AppState>,
+              private dataAction: DataAction) {
     PageScrollConfig.defaultDuration = 350;
+
+    //TODO: refactor this
+    let jwtHelper: JwtHelper = new JwtHelper();
+    let jwtToken = localStorage.getItem('id_token');
+    if (jwtToken) {
+      if (jwtHelper.isTokenExpired(jwtToken))
+        this.goToDash();
+      else
+        this.store.dispatch(this.dataAction.login(new Account(jwtHelper.decodeToken(jwtToken))))
+    } else this.goToDash();
+
   }
 
   private isLoginOrRegisterRoute(): boolean {
@@ -26,4 +44,12 @@ export class AppComponent {
       this.router.routerState.snapshot.url != '/register'
   }
 
+
+  private goToDash() {
+    this.router.navigate(['/']);
+  }
+
+  private goToHome() {
+    this.router.navigate(['/topics']);
+  }
 }
